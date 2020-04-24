ee6a56aa018b4cb3a896c44337c9f5a407718b48
hwpowell <hwpowell13@gmail.com>
2020-04-24 18:20:12 -0500

    Update language.js

--- language.js
+++ language.js
@@ -1,56 +1,128 @@
-function languageSelector(region='US', country='US'){
+function languageSelector(region="US", country="US"){
 
-	Qualtrics.SurveyEngine.setEmbeddedData('Delimiter' ,',');
-	var lang = jQuery('#Q_lang').val();
+	Qualtrics.SurveyEngine.setEmbeddedData("Delimiter" ,",");
+	var lang = jQuery("#Q_lang").val();
 	
 	switch (region){
-		case 'EU':
+		case "EU":
 			jQuery(".LanguageSelectorContainer").show();
-			jQuery('#Q_lang option[value='+'EN'+']').remove();
-			jQuery('#Q_lang option[value='+'EN-AU'+']').remove();
+			jQuery("#Q_lang option[value="+"EN"+"]").remove();
+			jQuery("#Q_lang option[value="+"EN-AU"+"]").remove();
 	
-			if (country == 'DE'){
-				Qualtrics.SurveyEngine.setEmbeddedData('Delimiter' ,'.');
-				if (lang != 'EN-GB' && lang != 'DE' && lang != 'NL'){
-					jQuery('#Q_lang').val('DE');
+			if (country == "DE"){
+				Qualtrics.SurveyEngine.setEmbeddedData("Delimiter" ,".");
+				if (lang != "EN-GB" && lang != "DE" && lang != "NL"){
+					jQuery("#Q_lang").val("DE");
 				}
 			}
-			else if (country == 'NL'){
-				if (lang != 'EN-GB' && lang != 'DE' && lang != 'NL'){
+			else if (country == "NL"){
+				if (lang != "EN-GB" && lang != "DE" && lang != "NL"){
 					jQuery('#Q_lang').val('NL');
 				}
 			}
 			else{
-				if (lang != 'EN-GB' && lang != 'DE' && lang != 'NL'){
-					jQuery('#Q_lang').val('EN-GB');
+				if (lang != "EN-GB" && lang != "DE" && lang != "NL"){
+					jQuery("#Q_lang").val("EN-GB");
 				}
 			}
 			break;
 		
-		case 'AU':
-			Qualtrics.SurveyEngine.setEmbeddedData('Sign' ,'$');
-			jQuery('#Q_lang').val('EN-AU');
+		case "AU":
+			Qualtrics.SurveyEngine.setEmbeddedData("Sign" ,"$");
+			jQuery("#Q_lang").val("EN-AU");
 			
-			if (country == 'NZ'){
-				Qualtrics.SurveyEngine.setEmbeddedData('Cr' ,'NZD');
+			if (country == "NZ"){
+				Qualtrics.SurveyEngine.setEmbeddedData("Cr" ,"NZD");
 			}
 			else {
-				Qualtrics.SurveyEngine.setEmbeddedData('Cr' ,'AUD');
+				Qualtrics.SurveyEngine.setEmbeddedData("Cr" ,"AUD");
 			}
 			break;
 		
 		default:
-			Qualtrics.SurveyEngine.setEmbeddedData('Cr' ,'USD');
-			Qualtrics.SurveyEngine.setEmbeddedData('Sign' ,'$');
-			jQuery('#Q_lang').val('EN');
+			Qualtrics.SurveyEngine.setEmbeddedData("Cr" ,"USD");
+			Qualtrics.SurveyEngine.setEmbeddedData("Sign" ,"$");
+			jQuery("#Q_lang").val("EN");
 			break;
 	}
 };
 
+
+function translations(language){
+	switch (language){
+		case "DE":
+			var dict = {
+				//Core
+				"EmailText" : "E-Mail-Adresse",
+				"PasswordText" : "Passwort",
+				"Location" : "Location",
+				"PostValid" : "Please enter a valid postal code",
+				"PostError" : " is not a valid postal code",
+				"NewClientsError" : "For new clients, you must indicate a value greater than or equal to:",
+				"LostClientsError" : "Lost clients must equal:",
+				//EX and Comp
+				"ExIntroText" : "Update here",
+				//M&A
+				"Months" : "Months",
+				// All modules
+				"SubmitSurvey" : "SUBMIT SURVEY"
+			};
+			break;
+		
+		case "NL":
+			var dict = {
+				//Core
+				"EmailText" : "E-mailadres",
+				"PasswordText" : "Wachtwoord",
+				"Location" : "Location",
+				"PostValid" : "Please enter a valid postal code",
+				"PostError" : " is not a valid postal code",
+				"NewClientsError" : "For new clients, you must indicate a value greater than or equal to:",
+				"LostClientsError" : "Lost clients must equal:",
+				//EX and Comp
+				"ExIntroText" : "Update here",
+				//M&A
+				"Months" : "Months",
+				// All modules
+				"SubmitSurvey" : "SUBMIT SURVEY"
+			};
+			break;
+		
+		default:
+			var dict = {
+				//Core
+				"EmailText" : "Email",
+				"PasswordText" : "Password",
+				"Location" : "Location",
+				"PostValid" : "Please enter a valid postal code",
+				"PostError" : " is not a valid postal code",
+				"NewClientsError" : "For new clients, you must indicate a value greater than or equal to:",
+				"LostClientsError" : "Lost clients must equal:",
+				//EX and Comp
+				"ExIntroText" : "This survey requires a password.",
+				//M&A
+				"Months" : "Months",
+				// All modules
+				"SubmitSurvey" : "SUBMIT SURVEY"
+			};
+			break;
+	};
+	
+	return dict;
+};
+
+
 function languageSettings(region, country){
 	
 	jQuery(document).ready(function(){
-		if (jQuery("[id^=Authenticator]").length !=0){
+		if (jQuery("[id^='Authenticator']").length !=0){
+			jQuery(".LanguageSelectorContainer").show();
+			var dict = translations(jQuery("#Q_lang").val());
+			jQuery("[id$='0~label']").html(dict["EmailText"]);
+			jQuery("[id$='1~label']").html(dict["PasswordText"]);
+			
+		}
+		else if (jQuery("#Password").length !=0){
 			jQuery(".LanguageSelectorContainer").show();
 		}
 		else {
